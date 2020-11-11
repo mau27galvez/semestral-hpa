1280bcd82db45d9995d5b9bb597c61ff7c1bf716
Mauricio Galvez <mauricio28galvez@gmail.com>
2020-11-11 09:27:06 -0500

    initial commit

---
+++ main.c
@@ -0,0 +1,24 @@
+/*
+En los estacionamientos del edificio N°3 en el Campus Central de la UTP, existe un espacio
+para ser alquilados a los docentes. La administración aplicar nuevas reglas y probar con un
+prototipo de control para la venta de los espacios de estacionamientos. Existe disponibilidad
+para 100 espacios y estos espacios se alquilarán semestralmente de acuerdo al tipo de auto
+(Hatchback $15, Sedan $25, SUV $35 y Todoterreno $45). El prototipo debe almacenar la
+información del auto y del docente; además, tener en cuenta un reporte de ventas.
+(Funcionamiento para un semestre).
+*/
+
+#define PARKING_SLOTS 100
+#define HATCHBACK_PRICE 15
+#define SEDAN_PRICE 15
+#define SUV_PRICE 15
+#define OFF_ROAD_PRICE 15
+
+
+
+int main()
+{
+
+
+    return 0;
+}
